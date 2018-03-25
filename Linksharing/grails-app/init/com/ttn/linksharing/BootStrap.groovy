c3a2e9834d65ea57dbfa023ae82df21f4f162547
Prachi <prachijulka31@gmail.com>
2018-03-25 14:08:01 +0530

    Question2flushTrue

--- Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
+++ Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
@@ -17,13 +17,13 @@ class BootStrap {
         //admin
         User admin=new User(email: "admin@gmail.com",password: "admin@123",firstName: "admin",lastName: "portal",userName:'adminPortal' , photo: 121,admin:true,active:true)
         admin.validate()
-        admin.save(failOnError:true)
+        admin.save(flush:true)
         if(admin.hasErrors()==true)
             return false
         //normal
         User normal=new User(email: "prachijulka@gmail.com",password: "admin@123",firstName: "Prachi",lastName: "Julka",userName:'PrachiJulka',photo: 122,admin:false,active:true)
         normal.validate()
-         normal.save(failOnError:true)
+         normal.save(flush:true)
 
         if(normal.hasErrors()==true)
             return false
