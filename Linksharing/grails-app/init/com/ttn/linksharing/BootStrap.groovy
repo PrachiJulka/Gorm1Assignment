c44db1fcd27a4356fb24af3128bb186dffa080b8
Prachi <prachijulka31@gmail.com>
2018-03-25 14:05:48 +0530

    Question2FailOnError

--- Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
+++ Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
@@ -1,6 +1,6 @@
 package com.ttn.linksharing
 /*
-Add createUsers method to Bootstrap to create 1 admin and 1 normal user.*/
+Use failOnError and flush true for persisting users.*/
 
 
 class BootStrap {
@@ -17,15 +17,13 @@ class BootStrap {
         //admin
         User admin=new User(email: "admin@gmail.com",password: "admin@123",firstName: "admin",lastName: "portal",userName:'adminPortal' , photo: 121,admin:true,active:true)
         admin.validate()
-        admin.save(flush:true)
-        log.error "error: ${admin.errors.getFieldError()}"
+        admin.save(failOnError:true)
         if(admin.hasErrors()==true)
             return false
         //normal
         User normal=new User(email: "prachijulka@gmail.com",password: "admin@123",firstName: "Prachi",lastName: "Julka",userName:'PrachiJulka',photo: 122,admin:false,active:true)
         normal.validate()
-         normal.save(flush:true)
-        log.error "error: ${normal.errors.getFieldError()}"
+         normal.save(failOnError:true)
 
         if(normal.hasErrors()==true)
             return false
