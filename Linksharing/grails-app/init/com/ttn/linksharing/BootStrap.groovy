08be3109b03675cd815941bc9cf149c57952b35c
Prachi <prachijulka31@gmail.com>
2018-03-25 14:14:49 +0530

    Question3

--- Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
+++ Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
@@ -17,12 +17,16 @@ class BootStrap {
         //admin
         User admin=new User(email: "admin@gmail.com",password: "admin@123",firstName: "admin",lastName: "portal",userName:'adminPortal' , photo: 121,admin:true,active:true)
         admin.validate()
+        log.error("error: ${admin.errors.getFieldErrors()}")
         admin.save(flush:true)
         if(admin.hasErrors()==true)
             return false
+
         //normal
         User normal=new User(email: "prachijulka@gmail.com",password: "admin@123",firstName: "Prachi",lastName: "Julka",userName:'PrachiJulka',photo: 122,admin:false,active:true)
         normal.validate()
+
+        log.error("error: ${normal.errors.getFieldErrors()}")
          normal.save(flush:true)
 
         if(normal.hasErrors()==true)
