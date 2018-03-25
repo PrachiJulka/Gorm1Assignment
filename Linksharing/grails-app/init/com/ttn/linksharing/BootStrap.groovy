9539b69f67c766046b440b778e782fc68b803cd3
Prachi <prachijulka31@gmail.com>
2018-03-25 15:17:42 +0530

    Question4

--- Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
+++ Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
@@ -7,32 +7,36 @@ class BootStrap {
 
     def init = { servletContext ->
         if(!createUsers())
-            println("Error in users creation")
+            log.error("Error in users creation")
         else
-        println("Users Created Successfully")
+        log.error("Users Created Successfully")
 
           }
     boolean createUsers(){
 
+
         //admin
-        User admin=new User(email: "admin@gmail.com",password: "admin@123",firstName: "admin",lastName: "portal",userName:'adminPortal' , photo: 121,admin:true,active:true)
-        admin.validate()
-        log.error("error: ${admin.errors.getFieldErrors()}")
-        admin.save(flush:true)
-        if(admin.hasErrors()==true)
-            return false
+        if(User.count()==0) {
+            User admin = new User(email: "admin@gmail.com", password: "admin@123", firstName: "admin", lastName: "portal", userName: 'adminPortal', photo: 121, admin: true, active: true)
+            admin.validate()
+            log.error("error: ${admin.errors.getFieldErrors()}")
+            admin.save(flush: true)
+            if (admin.hasErrors() == true)
+                return false
 
-        //normal
-        User normal=new User(email: "prachijulka@gmail.com",password: "admin@123",firstName: "Prachi",lastName: "Julka",userName:'PrachiJulka',photo: 122,admin:false,active:true)
-        normal.validate()
+            //normal
+            User normal = new User(email: "prachijulka@gmail.com", password: "admin@123", firstName: "Prachi", lastName: "Julka", userName: 'PrachiJulka', photo: 122, admin: false, active: true)
+            normal.validate()
 
-        log.error("error: ${normal.errors.getFieldErrors()}")
-         normal.save(flush:true)
+            log.error("error: ${normal.errors.getFieldErrors()}")
+            normal.save(flush: true)
 
-        if(normal.hasErrors()==true)
-            return false
+            if (normal.hasErrors() == true)
+                return false
 
-        return true
+            return true
+        }
+        return false
 
 
     }
