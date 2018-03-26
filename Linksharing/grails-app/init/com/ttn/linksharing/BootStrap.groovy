14fd1f6defb4f6032b58c447b0f059959f204b9e
PrachiJulka <prachijulka31@gmail.com>
2018-03-26 22:06:33 +0530

    Update BootStrap.groovy

--- Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
+++ Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
@@ -6,31 +6,25 @@ Add createUsers method to Bootstrap to create 1 admin and 1 normal user.*/
 class BootStrap {
 
     def init = { servletContext ->
-        if(!createUsers())
-            println("Error in users creation")
-        else
-        println("Users Created Successfully")
-
+       createUsers()
           }
-    boolean createUsers(){
+    void createUsers(){
 
         //admin
         User admin=new User(email: "admin@gmail.com",password: "admin@123",firstName: "admin",lastName: "portal",userName:'adminPortal' , photo: 121,admin:true,active:true)
         admin.validate()
+         log.error "error: ${admin.errors.getFieldError()}"
         admin.save(flush:true)
-        log.error "error: ${admin.errors.getFieldError()}"
-        if(admin.hasErrors()==true)
-            return false
+       
+       
         //normal
         User normal=new User(email: "prachijulka@gmail.com",password: "admin@123",firstName: "Prachi",lastName: "Julka",userName:'PrachiJulka',photo: 122,admin:false,active:true)
         normal.validate()
+          log.error "error: ${normal.errors.getFieldError()}"
          normal.save(flush:true)
-        log.error "error: ${normal.errors.getFieldError()}"
+      
 
-        if(normal.hasErrors()==true)
-            return false
-
-        return true
+       
 
 
     }
