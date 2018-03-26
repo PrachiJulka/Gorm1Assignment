24366d97d3e51fc504e588c076cc46f269fdc6aa
PrachiJulka <prachijulka31@gmail.com>
2018-03-26 22:14:21 +0530

    Update BootStrap.groovy

--- Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
+++ Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
@@ -6,34 +6,25 @@ Use log info statements for data creation and log error if there are any errors*
 class BootStrap {
 
     def init = { servletContext ->
-        if(!createUsers())
-            println("Error in users creation")
-        else
-        println("Users Created Successfully")
+      createUsers()
 
           }
-    boolean createUsers(){
+    void createUsers(){
 
-        //admin
-        User admin=new User(email: "admin@gmail.com",password: "admin@123",firstName: "admin",lastName: "portal",userName:'adminPortal' , photo: 121,admin:true,active:true)
-        admin.validate()
-        log.error("error: ${admin.errors.getFieldErrors()}")
-        admin.save(flush:true)
-        if(admin.hasErrors()==true)
-            return false
+       User admin = new User(email: "admin@gmail.com", password: "admin@123", firstName: "admin", lastName: "portal", userName: 'adminPortal', photo: 121, admin: true, active: true)
+            admin.validate()
+            log.error("error: ${admin.errors.getAllErrors()}")
+            admin.save(flush: true)
+            if(admin.errors.hasErrors()==false)
+                log.info("Admin Saved Successfully")
 
-        //normal
-        User normal=new User(email: "prachijulka@gmail.com",password: "admin@123",firstName: "Prachi",lastName: "Julka",userName:'PrachiJulka',photo: 122,admin:false,active:true)
-        normal.validate()
-
-        log.error("error: ${normal.errors.getFieldErrors()}")
-         normal.save(flush:true)
-
-        if(normal.hasErrors()==true)
-            return false
-
-        return true
-
+            //normal
+            User normal = new User(email: "prachijulka@gmail.com", password: "admin@123", firstName: "Prachi", lastName: "Julka", userName: 'PrachiJulka', photo: 122, admin: false, active: true)
+            normal.validate()
+            log.error("error: ${normal.errors.getFieldErrors()}")
+            normal.save(flush: true)
+            if(normal.errors.hasErrors()==false)
+                log.info("Normal User Saved Successfully")
 
     }
     def destroy = {
