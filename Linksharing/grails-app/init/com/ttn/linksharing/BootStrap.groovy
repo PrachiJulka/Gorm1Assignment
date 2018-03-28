8fd5683b5fe142b3b03f4603d3b9d9557b690cbe
PrachiJulka <prachijulka31@gmail.com>
2018-03-28 19:24:17 +0530

    Update BootStrap.groovy

--- Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
+++ Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
@@ -6,35 +6,83 @@ Add createTopics method in bootstrap*/
 class BootStrap {
 
     def init = { servletContext ->
-       createUsers()
+        createUsers()
+        createTopics()
           }
-    void createUsers(){
+     void createUsers(){
 
 
         //admin
         if(User.count()==0) {
-            User admin = new User(email: "admin@gmail.com", password: "admin@123", firstName: "admin", lastName: "portal", userName: 'adminPortal', photo: 121, admin: true, active: true)
-            admin.validate()
-            log.error("error: ${admin.errors.getAllErrors()}")
-            admin.save(flush: true)
-            if(admin.errors.hasErrors()==false)
+            User admin = new User(email: "admin@gmail.com", password: DefaultPassword.PASSWORD, firstName: "admin", lastName: "portal", userName: 'adminPortal', photo: 121, admin: true, active: true)
+            if(admin.save()){
                 log.info("Admin Saved Successfully")
+            }
+            else {
+                log.error("error: ${admin.errors.getAllErrors()}")
+            }
 
             //normal
-            User normal = new User(email: "prachijulka@gmail.com", password: "admin@123", firstName: "Prachi", lastName: "Julka", userName: 'PrachiJulka', photo: 122, admin: false, active: true)
-            normal.validate()
-            log.error("error: ${normal.errors.getFieldErrors()}")
-            normal.save(flush: true)
-            if(normal.errors.hasErrors()==false)
+            User normal = new User(email: "prachijulka@gmail.com", password: DefaultPassword.PASSWORD, firstName: "Prachi", lastName: "Julka", userName: 'PrachiJulka', photo: 122, admin: false, active: true)
+            if(normal.save()){
                 log.info("Normal User Saved Successfully")
+            }
+            else {
+                log.error("error: ${normal.errors.getFieldErrors()}")
+            }
+
         }
     }
 
+    void createTopics() {
+        List<User> userCount = User.getAll()
+        userCount.each {
 
-   void createTopic(){
+            if (Topic.findAllByCreatedBy(it).size()==0) {
 
-     
+                Topic topic = new Topic(name: "BigData", createdBy: it, visibility: Visibility.PUBLIC)
+
+                Topic topic1 = new Topic(name: "Java", createdBy: it, visibility: Visibility.PRIVATE)
+
+                Topic topic2 = new Topic(name: "nodeJs", createdBy: it, visibility: Visibility.PUBLIC)
+
+                Topic topic3 = new Topic(name: "AngularJs", createdBy: it, visibility: Visibility.PRIVATE)
+
+                Topic topic4 = new Topic(name: "MachineLearning", createdBy: it, visibility: Visibility.PUBLIC)
+
+
+                if(topic.save())
+                    it.addToTopics(topic)
+
+                if(topic1.save())
+                    it.addToTopics(topic1)
+
+                if(topic2.save())
+                    it.addToTopics(topic2)
+
+                if(topic3.save())
+                    it.addToTopics(topic3)
+
+                if(topic4.save())
+                    it.addToTopics(topic4)
+
+
+                else {
+                    log.error("Topic ${topic.errors.getFieldErrors()}")
+                    log.error("Topic ${topic1.errors.getFieldErrors()}")
+                    log.error("Topic ${topic2.errors.getFieldErrors()}")
+                    log.error("Topic ${topic3.errors.getFieldErrors()}")
+                    log.error("Topic ${topic4.errors.getFieldErrors()}")
+
+                }
+                it.save()
+                log.info("Topics Saved Successfully")
+
+
+            }
+        }
     }
+  
     def destroy = {
     }
 }
