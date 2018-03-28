af4a4b28f58e2953633e25a455cecbcd46d1ab1d
PrachiJulka <prachijulka31@gmail.com>
2018-03-28 19:29:40 +0530

    Update BootStrap.groovy

--- Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
+++ Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
@@ -10,25 +10,28 @@ class BootStrap {
         createTopics()
         createResource()
           }
-   void createUsers(){
+    void createUsers(){
 
 
         //admin
         if(User.count()==0) {
-            User admin = new User(email: "admin@gmail.com", password: "admin@123", firstName: "admin", lastName: "portal", userName: 'adminPortal', photo: 121, admin: true, active: true)
-            admin.validate()
-            log.error("error: ${admin.errors.getAllErrors()}")
-            admin.save(flush: true)
-            if(admin.errors.hasErrors()==false)
+            User admin = new User(email: "admin@gmail.com", password: DefaultPassword.PASSWORD, firstName: "admin", lastName: "portal", userName: 'adminPortal', photo: 121, admin: true, active: true)
+            if(admin.save()){
                 log.info("Admin Saved Successfully")
+            }
+            else {
+                log.error("error: ${admin.errors.getAllErrors()}")
+            }
 
             //normal
-            User normal = new User(email: "prachijulka@gmail.com", password: "admin@123", firstName: "Prachi", lastName: "Julka", userName: 'PrachiJulka', photo: 122, admin: false, active: true)
-            normal.validate()
-            log.error("error: ${normal.errors.getFieldErrors()}")
-            normal.save(flush: true)
-            if(normal.errors.hasErrors()==false)
+            User normal = new User(email: "prachijulka@gmail.com", password: DefaultPassword.PASSWORD, firstName: "Prachi", lastName: "Julka", userName: 'PrachiJulka', photo: 122, admin: false, active: true)
+            if(normal.save()){
                 log.info("Normal User Saved Successfully")
+            }
+            else {
+                log.error("error: ${normal.errors.getFieldErrors()}")
+            }
+
         }
     }
 
@@ -37,43 +40,50 @@ class BootStrap {
         userCount.each {
 
             if (Topic.findAllByCreatedBy(it).size()==0) {
-                //User normal = User.findByUserName("PrachiJulka")
 
                 Topic topic = new Topic(name: "BigData", createdBy: it, visibility: Visibility.PUBLIC)
-                it.addToTopics(topic)
 
                 Topic topic1 = new Topic(name: "Java", createdBy: it, visibility: Visibility.PRIVATE)
-                it.addToTopics(topic1)
 
                 Topic topic2 = new Topic(name: "nodeJs", createdBy: it, visibility: Visibility.PUBLIC)
-                it.addToTopics(topic2)
 
                 Topic topic3 = new Topic(name: "AngularJs", createdBy: it, visibility: Visibility.PRIVATE)
-                it.addToTopics(topic3)
 
                 Topic topic4 = new Topic(name: "MachineLearning", createdBy: it, visibility: Visibility.PUBLIC)
-                it.addToTopics(topic4)
 
-                topic.validate()
-                log.error("Topic ${topic.errors.getFieldErrors()}")
-                topic.save()
-                topic1.validate()
-                log.error("Topic ${topic1.errors.getFieldErrors()}")
-                topic1.save()
-                topic2.validate()
-                log.error("Topic ${topic2.errors.getFieldErrors()}")
-                topic2.save()
-                topic3.validate()
-                log.error("Topic ${topic3.errors.getFieldErrors()}")
-                topic3.save()
-                topic4.validate()
-                log.error("Topic ${topic4.errors.getFieldErrors()}")
-                topic4.save()
+
+                if(topic.save())
+                    it.addToTopics(topic)
+
+                if(topic1.save())
+                    it.addToTopics(topic1)
+
+                if(topic2.save())
+                    it.addToTopics(topic2)
+
+                if(topic3.save())
+                    it.addToTopics(topic3)
+
+                if(topic4.save())
+                    it.addToTopics(topic4)
+
+
+                else {
+                    log.error("Topic ${topic.errors.getFieldErrors()}")
+                    log.error("Topic ${topic1.errors.getFieldErrors()}")
+                    log.error("Topic ${topic2.errors.getFieldErrors()}")
+                    log.error("Topic ${topic3.errors.getFieldErrors()}")
+                    log.error("Topic ${topic4.errors.getFieldErrors()}")
+
+                }
+                it.save()
+                log.info("Topics Saved Successfully")
+
 
             }
         }
     }
-            void createResource(){
+        void createResource(){
 
             if(Resource.count()==0) {
 
@@ -81,22 +91,44 @@ class BootStrap {
 
                 topics.each {
                     Resource resource = new LinkResource(url: "https://en.wikipedia.org/wiki/Big_data", description: "${it.name} url", topic: it, user: it.createdBy)
-                    resource.validate()
-                    log.error("Resource Error: ${resource.errors.allErrors}")
-                    resource.save()
-                    Resource resource1 = new LinkResource(url: "https://www.sas.com/en_in/insights/big-data/what-is-big-data.html", description: "${it.name} bigdata", topic: it, user:it.createdBy)
-                    resource1.validate()
-                    log.error("Resource Error: ${resource1.errors.allErrors}")
-                    resource1.save()
+                    Resource resource1 = new LinkResource(url: "https://www.sas.com/en_in/insights/big-data/what-is-big-data.html", description: "${it.name} bigdata", topic: it, user: it.createdBy)
                     Resource resource2 = new DocumentResource(filePath: "fvnkdfvdk", description: "${it.name} cndfbcfefbfer", user: it.createdBy, topic: it)
-                    resource2.validate()
-                    log.error("Resource Error: ${resource2.errors.allErrors}")
-                    resource2.save()
                     Resource resource3 = new DocumentResource(filePath: "nvdjfn", user: it.createdBy, description: "${it.name} sdns", topic: it)
-                    resource3.validate()
-                    log.error("Resource Error: ${resource3.errors.allErrors}")
-                    resource3.save()
 
+                    if (resource.save()) {
+                        it.addToResources(resource)
+                        it.createdBy.addToResources(resource)
+                    }
+                    else {
+                        log.error("Resource Error: ${resource.errors.allErrors}")
+                    }
+                    if (resource1.save()) {
+                        it.addToResources(resource1)
+                        it.createdBy.addToResources(resource1)
+                    }
+                    else {
+                        log.error("Resource Error: ${resource1.errors.allErrors}")
+                    }
+                    if(resource2.save()){
+                        it.addToResources(resource2)
+                        it.createdBy.addToResources(resource2)
+
+                    }
+                    else{
+                        log.error("Resource Error: ${resource2.errors.allErrors}")
+
+                    }
+                    if(resource3.save()){
+                        it.addToResources(resource3)
+                        it.createdBy.addToResources(resource3)
+
+                    }
+                    else{
+                        log.error("Resource Error: ${resource3.errors.allErrors}")
+                      }
+                    createReadingItemIfItDoesNotExistsInUsersReadingItem(it.createdBy,it)
+                    it.createdBy.save()
+                    it.save()
                 }
 
             }
