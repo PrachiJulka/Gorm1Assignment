5da68aa357355c8525bcc39b67261c024f40d16a
PrachiJulka <prachijulka31@gmail.com>
2018-03-28 19:57:14 +0530

    Update BootStrap.groovy

--- Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
+++ Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
@@ -150,7 +150,6 @@ class BootStrap {
                 if(Subscription.findAllByTopicsAndUser(it,user).size()==0) {
                     Subscription subscription = new Subscription(seriousness: Seriousness.CASUAL, user: user, topics: it)
                     if (subscription.save()) {
-                        createReadingItemIfItDoesNotExistsInUsersReadingItem(subscription.user,subscription.topics)
                         it.addToSubscriptions(subscription)
                         user.addToSubscriptions(subscription)
                     }
