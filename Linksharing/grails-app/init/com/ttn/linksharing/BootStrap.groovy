49b78facf965ad5ab7e0d726cc9ccf62181157b7
PrachiJulka <prachijulka31@gmail.com>
2018-03-28 19:17:09 +0530

    Update BootStrap.groovy

--- Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
+++ Linksharing/grails-app/init/com/ttn/linksharing/BootStrap.groovy
@@ -8,25 +8,29 @@ class BootStrap {
     def init = { servletContext ->
        createUsers()
           }
-    void createUsers(){
+     void createUsers(){
+
 
         //admin
-        User admin=new User(email: "admin@gmail.com",password: "admin@123",firstName: "admin",lastName: "portal",userName:'adminPortal' , photo: 121,admin:true,active:true)
-        admin.validate()
-         log.error "error: ${admin.errors.getFieldError()}"
-        admin.save(flush:true)
-       
-       
-        //normal
-        User normal=new User(email: "prachijulka@gmail.com",password: "admin@123",firstName: "Prachi",lastName: "Julka",userName:'PrachiJulka',photo: 122,admin:false,active:true)
-        normal.validate()
-          log.error "error: ${normal.errors.getFieldError()}"
-         normal.save(flush:true)
-      
+        if(User.count()==0) {
+            User admin = new User(email: "admin@gmail.com", password: DefaultPassword.PASSWORD, firstName: "admin", lastName: "portal", userName: 'adminPortal', photo: 121, admin: true, active: true)
+            if(admin.save()){
+                log.info("Admin Saved Successfully")
+            }
+            else {
+                log.error("error: ${admin.errors.getAllErrors()}")
+            }
 
-       
+            //normal
+            User normal = new User(email: "prachijulka@gmail.com", password: DefaultPassword.PASSWORD, firstName: "Prachi", lastName: "Julka", userName: 'PrachiJulka', photo: 122, admin: false, active: true)
+            if(normal.save()){
+                log.info("Normal User Saved Successfully")
+            }
+            else {
+                log.error("error: ${normal.errors.getFieldErrors()}")
+            }
 
-
+        }
     }
     def destroy = {
     }
